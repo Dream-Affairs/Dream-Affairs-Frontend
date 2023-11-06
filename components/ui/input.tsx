8172ba6b5bf896023fd07e42573b47cef76d91af
Prosper Agwegiokhe <54757652+Phingaz@users.noreply.github.com>
2023-11-06 02:17:51 -0300

    bugs and fixes

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -17,13 +17,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            `flex h-full w-full rounded-lg border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-400 disabled:cursor-not-allowed disabled:opacity-50 outline-none ring-0  focus:border-muted transition-colors duration-200 ease-in-out hover:bg-accent hover:text-primary hover:border-accent hover:placeholder-primary
+            `flex h-full w-full rounded-lg bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-400 disabled:cursor-not-allowed disabled:opacity-50 outline-none ring-0  focus:border-muted transition-colors duration-200 ease-in-out hover:bg-accent hover:text-primary hover:border-accent hover:placeholder-primary
              ${error && 'border-red-500 bg-red-50 text-red-500 placeholder:text-red-500 focus:border-red-500'}
             
-            ${
-              hasValue &&
-              'bg-[hsl(232,52%,94%,0.5)] text-black placeholder:text-black focus:border-[hsl(232,52%,94%,0.5)]'
-            }
+            ${hasValue && 'bg-[hsl(232,52%,94%,0.5)] border-accent focus:border-[hsl(232,52%,94%,0.5)]'}
             `,
             className,
           )}
