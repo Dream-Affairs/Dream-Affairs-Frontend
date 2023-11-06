2146527f685a61dd1bcda9ff26e1746e64ed9b18
Prosper Agwegiokhe <54757652+Phingaz@users.noreply.github.com>
2023-11-05 23:03:04 -0300

    added some style guides

--- app/auth/(components)/DatePicker.tsx
+++ app/auth/(components)/DatePicker.tsx
@@ -8,7 +8,6 @@ import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { FaAngleDown } from 'react-icons/fa';
 
 interface DatePickerProps {
   error?: boolean;
