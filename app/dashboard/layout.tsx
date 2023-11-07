7cdfc9e3842d2de80356d22c910ab7601b239be4
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-11-07 12:42:49 +0100

    sidebar complete

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from './(components)/Sidebar'
+
+type Props = {}
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+      <main className='flex w-full transition-all duration-1000'>
+      <Sidebar />
+      <>
+          {children}
+      </>
+      </main>
+  )
+}
+
+export default DashboardLayout
