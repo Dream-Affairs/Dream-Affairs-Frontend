92982d2308a451c48e85b167e991015c6de63f97
VictorIdowu <idsonvictor@gmail.com>
2023-11-07 19:36:15 +0100

    Added add-task button

--- app/dashboard/checklist/page.tsx
+++ app/dashboard/checklist/page.tsx
@@ -3,9 +3,10 @@ import React, { useState } from 'react';
 import Filter from '../(components)/checklist/Filter';
 import Search from '../(components)/checklist/Search';
 import Task from '../(components)/checklist/Task';
+import { Button } from '@/components/ui/button';
 const Checklist = () => {
   return (
-    <section className="w-full px-14">
+    <section className="w-full px-8">
       <aside className="pt-10 flex flex-col gap-2 justify-start items-start">
         <h1 className="text-black text-4xl font-bold  leading-10">Wedding Checklist</h1>
         <p className="text-neutral-400 text-base font-normal  leading-snug">
@@ -20,7 +21,11 @@ const Checklist = () => {
           <h3 className="text-zinc-800 text-2xl font-semibold font-['Work Sans'] leading-loose">Tasks</h3>
           <div className="justify-center items-center gap-3 flex">
             <p className="text-zinc-800 leading-snug">Filter By</p>
-            <Filter />
+
+            <aside className="grid grid-cols-2 gap-3">
+              <Filter />
+              <Button variant="secondary">Add Task</Button>
+            </aside>
           </div>
         </div>
         <div className="w-full mt-8 border-t border-neutral-200">
