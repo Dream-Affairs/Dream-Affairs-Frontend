e900895c69586a1fe82b14f78df52c66410c1099
VictorIdowu <idsonvictor@gmail.com>
2023-11-08 01:07:27 +0100

    Fixed a few things

--- app/dashboard/(components)/checklist/Task.tsx
+++ app/dashboard/(components)/checklist/Task.tsx
@@ -1,71 +1,74 @@
 'use client';
+import { Button } from '@/components/ui/button';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import { Calendar } from '@/components/ui/calendar';
+import { Input } from '@/components/ui/input';
 const Task = () => {
+  const [date, setDate] = React.useState<Date | undefined>(undefined);
+
+  const error = false;
   const [textIsEditing, setTextIsEditing] = useState(false);
-  const [dateIsEditing, setDateIsEditing] = useState(false);
   const [taskText, setTaskText] = useState('');
-  const [dateText, setDateText] = useState('');
   useEffect(() => {
     taskText.length === 0 ? setTaskText('Create a New Task') : '';
-    dateText.length === 0 ? setDateText('Set due date') : '';
-  }, []);
+  }, [taskText]);
   return (
-    <div className="w-full items-center grid grid-cols-3 pt-4 pb-1 border-b border-neutral-200">
-      <aside className="flex gap-3">
+    <div className="w-full items-center grid grid-cols-3 pt-4 pb-1 border-b border-neutral-200 ">
+      <aside className="flex gap-3 ">
         <div className="min-w-[20px] h-5 mt-1 border-[3px] border-[#9F7DB5] rounded-[50%]"> </div>
         <div className="w-96 flex-col justify-start items-start gap-2 flex">
-          {textIsEditing ? (
-            <input
-              className="focus:outline-none text-neutral-400"
-              type="text"
-              value={taskText}
-              onChange={(e) => setTaskText(e.target.value)}
-              onBlur={() => {
-                setTextIsEditing(false);
-                taskText.length === 0 ? setTaskText('Create a New Task') : '';
-              }}
-              onKeyDown={(e) => {
-                if (e.key == 'Enter') {
-                  setTextIsEditing(false);
-                  taskText.length === 0 ? setTaskText('Create a New Task') : '';
-                }
-              }}
-            />
-          ) : (
-            <p
-              className={` text-base font-normal font-['Work Sans'] leading-snug ${
-                taskText !== 'Create a New Task' ? '' : 'text-neutral-400'
-              }`}
-              onClick={() => setTextIsEditing(true)}
+          <p
+            className={` text-base font-normal font-['Work Sans'] leading-snug relative ${
+              taskText !== 'Create a New Task' ? '' : 'text-neutral-400'
+            }`}
+            onClick={() => setTextIsEditing(true)}
+          >
+            {taskText}
+            {textIsEditing && (
+              <div className={`absolute left-0 top-5 w-[200px] `}>
+                <Input
+                  type="text"
+                  placeholder="Create a New Task"
+                  value={taskText === 'Create a New Task' ? '' : taskText}
+                  hasValue={taskText.length > 0}
+                  onChange={(e) => setTaskText(e.target.value)}
+                  autoFocus
+                  onBlur={() => {
+                    setTextIsEditing(false);
+                  }}
+                  onKeyDown={(e) => {
+                    if (e.key == 'Enter') {
+                      setTextIsEditing(false);
+                    }
+                  }}
+                />
+              </div>
+            )}
+          </p>
+
+          <Popover>
+            <PopoverTrigger
+              className={cn(
+                ` text-sm  file:text-sm file:font-medium placeholder:text-gray-400 disabled:cursor-not-allowed disabled:opacity-50 outline-none ring-0 transition-colors duration-200 ease-in-out border-0 hover:!bg-none
+           ${error && ' text-red-500 placeholder:text-red-500'}
+      
+          ${date && ' text-black'}`,
+              )}
+              asChild
             >
-              {taskText}
-            </p>
-          )}
-          {dateIsEditing ? (
-            <input
-              className="text-neutral-400 text-xs  leading-none"
-              type="date"
-              placeholder="Set due date"
-              id="date"
-              name="date"
-              value={dateText}
-              onChange={(e) => setDateText(e.target.value)}
-              onBlur={() => setDateIsEditing(false)}
-              onKeyDown={(e) => {
-                if (e.key == 'Enter') {
-                  setDateIsEditing(false);
-                  dateText.length === 0 ? setDateText('Set due date') : '';
-                }
-              }}
-            ></input>
-          ) : (
-            <p
-              className={`text-sm leading-none ${dateText === 'Set due date' ? 'text-neutral-400' : 'text-zinc-500'}`}
-              onClick={() => setDateIsEditing(true)}
-            >
-              {dateText}
-            </p>
-          )}
+              <p
+                className={cn('w-full justify-start text-zinc-500 text-left font-normal', !date && 'text-neutral-400')}
+              >
+                {date ? format(date, 'MMM dd') : <span>Set due date</span>}
+              </p>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto -translate-x-1/2 p-0">
+              <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+            </PopoverContent>
+          </Popover>
         </div>
       </aside>
       <svg
@@ -121,3 +124,31 @@ const Task = () => {
   );
 };
 export default Task;
+
+{
+  /* {dateIsEditing ? (
+            <input
+              className="text-neutral-400 text-xs  leading-none"
+              type="date"
+              placeholder="Set due date"
+              id="date"
+              name="date"
+              value={dateText}
+              onChange={(e) => setDateText(e.target.value)}
+              onBlur={() => setDateIsEditing(false)}
+              onKeyDown={(e) => {
+                if (e.key == 'Enter') {
+                  setDateIsEditing(false);
+                  dateText.length === 0 ? setDateText('Set due date') : '';
+                }
+              }}
+            ></input>
+          ) : (
+            <p
+              className={`text-sm leading-none ${dateText === 'Set due date' ? 'text-neutral-400' : 'text-zinc-500'}`}
+              onClick={() => setDateIsEditing(true)}
+            >
+              {dateText}
+            </p>
+          )} */
+}
--- app/dashboard/checklist/page.tsx
+++ app/dashboard/checklist/page.tsx
@@ -1,10 +1,25 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Filter from '../(components)/checklist/Filter';
 import Search from '../(components)/checklist/Search';
 import Task from '../(components)/checklist/Task';
 import { Button } from '@/components/ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+const filter: string[] = ['All tasks', 'Assigned to me', 'Assigned by me', 'Completed'];
+
 const Checklist = () => {
+  const [filterKey, setFilterKey] = useState('All tasks');
+  const [addTask, setAddTask] = useState(false);
+
   return (
     <section className="w-full px-8">
       <aside className="pt-10 flex flex-col gap-2 justify-start items-start">
@@ -23,14 +38,28 @@ const Checklist = () => {
             <p className="text-zinc-800 leading-snug">Filter By</p>
 
             <aside className="grid grid-cols-2 gap-3">
-              <Filter />
-              <Button variant="secondary">Add Task</Button>
+              {/* <Filter /> */}
+              <Select>
+                <SelectTrigger className="w-[180px] h-[55px]">
+                  <SelectValue placeholder={filterKey} />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {filter.map((item, i) => (
+                      <SelectItem value={item} key={i + 1} onClick={() => setFilterKey(item)}>
+                        {item}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Button variant="secondary" onClick={() => setAddTask((prev) => !prev)}>
+                Add Task
+              </Button>
             </aside>
           </div>
         </div>
-        <div className="w-full mt-8 border-t border-neutral-200">
-          <Task />
-        </div>
+        <div className="w-full mt-8 border-t border-neutral-200">{addTask && <Task />}</div>
       </aside>
     </section>
   );
