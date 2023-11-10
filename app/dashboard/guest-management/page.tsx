a93a55cba679031c9363d902053be532f56aa5f0
Olumide Bambe <loyalsfc@gmail.com>
2023-11-10 01:09:25 +0100

    Feat:Event management headers

--- app/dashboard/guest-management/page.tsx
+++ app/dashboard/guest-management/page.tsx
@@ -1,9 +1,177 @@
+import { Delete, HashTag, Import, Send, Store, Tags, Track } from '@/components/svg-icons/svg-icons';
+import { Button } from '@/components/ui/button';
+import { DeleteIcon } from 'lucide-react';
 import React from 'react';
 
 type Props = {};
 
 const GuestManagement = (props: Props) => {
-  return <div>GuestManagement</div>;
+  return (
+    <div className="pt-10 w-full h-full py-6 space-y-8">
+      <div className=" pl-[50px] pr-[50px] w-full overflow-hidden">
+        <header className="flex justify-between">
+          <h2 className="text-3xl font-bold text-[#1c1c1c]">Guest Management</h2>
+          <div className="flex gap-[22px]">
+            <Button variant="disabled" className="gap-2.5" size={'lg'}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+                <path
+                  d="M12.5 9V2L10.5 4"
+                  stroke="#9C9C9C"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M12.5 2L14.5 4"
+                  stroke="#9C9C9C"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M2.47998 13H6.88998C7.26998 13 7.60998 13.21 7.77998 13.55L8.94998 15.89C9.28998 16.57 9.97998 17 10.74 17H14.27C15.03 17 15.72 16.57 16.06 15.89L17.23 13.55C17.4 13.21 17.75 13 18.12 13H22.48"
+                  stroke="#9C9C9C"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M7.5 5.12988C3.96 5.64988 2.5 7.72988 2.5 11.9999V14.9999C2.5 19.9999 4.5 21.9999 9.5 21.9999H15.5C20.5 21.9999 22.5 19.9999 22.5 14.9999V11.9999C22.5 7.72988 21.04 5.64988 17.5 5.12988"
+                  stroke="#9C9C9C"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              Export
+            </Button>
+            <Button size="lg" variant="secondary">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M18.5 19.5H14.5"
+                  stroke="#282828"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M16.5 21.5V17.5"
+                  stroke="#282828"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M12.16 10.87C12.06 10.86 11.94 10.86 11.83 10.87C9.44997 10.79 7.55997 8.84 7.55997 6.44C7.54997 3.99 9.53997 2 11.99 2C14.44 2 16.43 3.99 16.43 6.44C16.43 8.84 14.53 10.79 12.16 10.87Z"
+                  stroke="#292D32"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M11.99 21.8097C10.17 21.8097 8.36004 21.3497 6.98004 20.4297C4.56004 18.8097 4.56004 16.1697 6.98004 14.5597C9.73004 12.7197 14.24 12.7197 16.99 14.5597"
+                  stroke="#292D32"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              Add Guest
+            </Button>
+          </div>
+        </header>
+        <div className="flex items-stretch gap-3.5 py-8">
+          <div className="flex items-center w-5/6 border border-[#D0D5DD] px-3 gap-2 rounded-md">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M9.58329 1.66699C5.21104 1.66699 1.66663 5.2114 1.66663 9.58366C1.66663 13.9559 5.21104 17.5003 9.58329 17.5003C11.4692 17.5003 13.2012 16.8409 14.5611 15.7399L16.9107 18.0896C17.2361 18.415 17.7638 18.415 18.0892 18.0896C18.4147 17.7641 18.4147 17.2365 18.0892 16.9111L15.7396 14.5614C16.8405 13.2015 17.5 11.4696 17.5 9.58366C17.5 5.2114 13.9555 1.66699 9.58329 1.66699ZM3.33329 9.58366C3.33329 6.13188 6.13151 3.33366 9.58329 3.33366C13.0351 3.33366 15.8333 6.13188 15.8333 9.58366C15.8333 13.0354 13.0351 15.8337 9.58329 15.8337C6.13151 15.8337 3.33329 13.0354 3.33329 9.58366Z"
+                fill="#8E8E8E"
+              />
+            </svg>
+            <input type="text" placeholder="Search for guests" className="focus:outline-none" />
+          </div>
+          <Button variant="outline" size="sm" className="gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <path
+                d="M2.79167 1.66699H10.2084C10.825 1.66699 11.3333 2.17533 11.3333 2.792V4.02531C11.3333 4.47531 11.05 5.03366 10.775 5.317L8.35838 7.45033C8.02504 7.73366 7.80001 8.29198 7.80001 8.74198V11.1587C7.80001 11.492 7.57502 11.942 7.29169 12.117L6.50835 12.6253C5.77502 13.0753 4.76666 12.567 4.76666 11.667V8.69199C4.76666 8.30032 4.54168 7.792 4.31668 7.50867L2.18335 5.25866C1.90002 4.97532 1.67503 4.47532 1.67503 4.13365V2.842C1.66669 2.17533 2.175 1.66699 2.79167 1.66699Z"
+                stroke="#292D32"
+                stroke-width="1.5"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M1.66663 9.99955V12.4996C1.66663 16.6662 3.33329 18.3329 7.49996 18.3329H12.5C16.6666 18.3329 18.3333 16.6662 18.3333 12.4996V7.49955C18.3333 4.89955 17.6833 3.26621 16.1749 2.41621C15.7499 2.17455 14.9 1.99121 14.125 1.86621"
+                stroke="#292D32"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M10.8334 10.833H15"
+                stroke="#292D32"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M9.16663 14.167H15"
+                stroke="#292D32"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            Filters
+          </Button>
+        </div>
+        <div className="w-full overflow-hidden">
+          <div className="flex items-center gap-3.5 w-full overflow-scroll no-scrollbar">
+            <Button variant="outline" size={'sm'} className="gap-2">
+              <Delete />
+              Delete Guest
+            </Button>
+            <Button size="sm" className="guest-btn">
+              <Import />
+              Import List
+            </Button>
+            <Button size="sm" className="guest-btn">
+              <Tags />
+              Assign Tags
+            </Button>
+            <Button size="sm" className="guest-btn">
+              <Store />
+              Manage Tags
+            </Button>
+            <Button size="sm" className="guest-btn">
+              <Track />
+              Track RSVP
+            </Button>
+            <Button size="sm" className="guest-btn">
+              <Send />
+              Send Invites
+            </Button>
+            <Button size="sm" className="guest-btn">
+              <Send />
+              Send Invites
+            </Button>
+            <Button size="sm" className="guest-btn">
+              <HashTag />
+              Populate Tables
+            </Button>
+          </div>
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GuestManagement;
---
+++ components/svg-icons/svg-icons.tsx
@@ -0,0 +1,236 @@
+import React from 'react';
+
+export function Delete() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M7.49996 18.3337H12.5C16.6666 18.3337 18.3333 16.667 18.3333 12.5003V7.50033C18.3333 3.33366 16.6666 1.66699 12.5 1.66699H7.49996C3.33329 1.66699 1.66663 3.33366 1.66663 7.50033V12.5003C1.66663 16.667 3.33329 18.3337 7.49996 18.3337Z"
+        stroke="#9C9C9C"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.9167 7.5416C13.1 7.35827 11.2667 7.2666 9.4417 7.2666C8.35837 7.2666 7.27504 7.32493 6.20004 7.43327L5.08337 7.5416"
+        stroke="#9C9C9C"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.09167 6.99121L8.20834 6.27454C8.29167 5.75787 8.35834 5.36621 9.28334 5.36621H10.7167C11.6417 5.36621 11.7083 5.77454 11.7917 6.27454L11.9083 6.98288"
+        stroke="#9C9C9C"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M13.7417 7.60742L13.3833 13.1074C13.325 13.9657 13.275 14.6324 11.75 14.6324H8.24167C6.71667 14.6324 6.66666 13.9657 6.60833 13.1074L6.25 7.60742"
+        stroke="#9C9C9C"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function Import() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M7.5 9.16699V14.167L9.16667 12.5003"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7.50004 14.1667L5.83337 12.5"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M18.3333 8.33366V12.5003C18.3333 16.667 16.6666 18.3337 12.5 18.3337H7.49996C3.33329 18.3337 1.66663 16.667 1.66663 12.5003V7.50033C1.66663 3.33366 3.33329 1.66699 7.49996 1.66699H11.6666"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M18.3333 8.33366H15C12.5 8.33366 11.6666 7.50033 11.6666 5.00033V1.66699L18.3333 8.33366Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function Tags() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M3.67063 12.9386L7.44563 16.7136C8.99563 18.2636 11.5123 18.2636 13.0706 16.7136L16.729 13.0553C18.279 11.5053 18.279 8.98864 16.729 7.43031L12.9456 3.66364C12.154 2.87197 11.0623 2.44697 9.94563 2.50531L5.77896 2.70531C4.1123 2.78031 2.7873 4.10531 2.70396 5.76364L2.50396 9.93031C2.45396 11.0553 2.87896 12.147 3.67063 12.9386Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.11226 10.1882C9.26286 10.1882 10.1956 9.25541 10.1956 8.10482C10.1956 6.95422 9.26286 6.02148 8.11226 6.02148C6.96167 6.02148 6.02893 6.95422 6.02893 8.10482C6.02893 9.25541 6.96167 10.1882 8.11226 10.1882Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M11.0289 14.3548L14.3623 11.0215"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function Store() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M2.5083 9.34961V13.0913C2.5083 16.8329 4.0083 18.3329 7.74997 18.3329H12.2416C15.9833 18.3329 17.4833 16.8329 17.4833 13.0913V9.34961"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.99999 10.0003C11.525 10.0003 12.65 8.75866 12.5 7.23366L11.95 1.66699H8.05832L7.49999 7.23366C7.34999 8.75866 8.47499 10.0003 9.99999 10.0003Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.2583 10.0003C16.9416 10.0003 18.175 8.63366 18.0083 6.95866L17.775 4.66699C17.475 2.50033 16.6416 1.66699 14.4583 1.66699H11.9166L12.5 7.50866C12.6416 8.88366 13.8833 10.0003 15.2583 10.0003Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4.70002 10.0003C6.07502 10.0003 7.31669 8.88366 7.45002 7.50866L7.63336 5.66699L8.03336 1.66699H5.49169C3.30836 1.66699 2.47502 2.50033 2.17502 4.66699L1.95002 6.95866C1.78336 8.63366 3.01669 10.0003 4.70002 10.0003Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.99996 14.167C8.60829 14.167 7.91663 14.8587 7.91663 16.2503V18.3337H12.0833V16.2503C12.0833 14.8587 11.3916 14.167 9.99996 14.167Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function Track() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M18.3333 6.19206V11.1921C18.3333 12.4421 17.9166 13.4837 17.1833 14.2171C16.4583 14.9421 15.4166 15.3587 14.1666 15.3587V17.1337C14.1666 17.8004 13.425 18.2004 12.875 17.8338L9.16663 15.3587H7.39996C7.46663 15.1087 7.49996 14.8504 7.49996 14.5837C7.49996 13.7337 7.17496 12.9504 6.64163 12.3587C6.04163 11.6754 5.14996 11.2504 4.16663 11.2504C3.23329 11.2504 2.3833 11.6337 1.77496 12.2587C1.69996 11.9254 1.66663 11.5671 1.66663 11.1921V6.19206C1.66663 3.69206 3.33329 2.02539 5.83329 2.02539H14.1666C16.6666 2.02539 18.3333 3.69206 18.3333 6.19206Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7.50004 14.5833C7.50004 15.5833 7.05839 16.475 6.36672 17.0833C5.77505 17.6 5.00837 17.9167 4.16671 17.9167C2.32504 17.9167 0.833374 16.425 0.833374 14.5833C0.833374 13.5333 1.31671 12.5917 2.08337 11.9833C2.65837 11.525 3.38337 11.25 4.16671 11.25C6.00837 11.25 7.50004 12.7417 7.50004 14.5833Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4.37504 13.542V14.792L3.33337 15.417"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7.08337 8.75H12.9167"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function Send() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M6.16665 5.2668L13.2417 2.90846C16.4167 1.85013 18.1417 3.58346 17.0917 6.75846L14.7333 13.8335C13.15 18.5918 10.55 18.5918 8.96665 13.8335L8.26665 11.7335L6.16665 11.0335C1.40832 9.45013 1.40832 6.85846 6.16665 5.2668Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.42505 11.3755L11.4084 8.38379"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function HashTag() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M8.33329 2.5L6.66663 17.5"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M13.3333 2.5L11.6666 17.5"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2.91663 7.5H17.9166"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2.08337 12.5H17.0834"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
