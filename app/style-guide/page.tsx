51d9eb9b3231d28f27d2be2545491b8ff3138ccb
Prosper Agwegiokhe <54757652+Phingaz@users.noreply.github.com>
2023-11-13 09:03:12 -0300

    bugs and fixes

--- app/style-guide/page.tsx
+++ app/style-guide/page.tsx
@@ -581,6 +581,25 @@ const ModalDemo = () => {
         </div>
       </Modal>
       <Modal
+        width="600"
+        showXIcon={false}
+        btnTiggerText={<BsCalendar2Date className="text-lg" />}
+        btnTriggerStyle="py-2 px-4 bg-secondary rounded-full"
+        showCloseBtn={true}
+        closeBtnText="Close"
+        closeBtnStyle="bg-secondary p-4 rounded-md text-sm font-medium w-full mt-5"
+      >
+        <div className="flex justify-center items-center gap-2 flex-col">
+          <h1 className="font-bold text-lg">Save The Date</h1>
+          <span className="bg-secondary p-2 rounded-full">
+            <BsCalendar2Date className="text-lg" />
+          </span>
+          <p className="text-center text-sm text-gray-400">
+            You have successfully sent out 1050 save the date wedding reminder to your guest list.
+          </p>
+        </div>
+      </Modal>
+      <Modal
         width="700"
         showXIcon={false}
         btnTiggerText={'Modal with another button width' + '700'}
