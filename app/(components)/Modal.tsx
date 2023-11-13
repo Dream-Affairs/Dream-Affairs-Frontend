aadd808d09ced91fcdbce822343666e2e2fbb676
Prosper Agwegiokhe <54757652+Phingaz@users.noreply.github.com>
2023-11-12 21:20:55 -0300

    modal

---
+++ app/(components)/Modal.tsx
@@ -0,0 +1,55 @@
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogClose, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { AiOutlineClose } from 'react-icons/ai';
+
+type Props = {
+  maxWidth: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl';
+  btnTiggerText: string;
+  btnTriggerStyle: string;
+  showXIcon: boolean;
+  showCloseBtn?: boolean;
+  closeBtnText?: string;
+  closeBtnStyle?: string;
+  otherBtn?: React.ReactNode;
+  children: React.ReactNode;
+};
+
+export function Modal({
+  showXIcon,
+  maxWidth,
+  btnTiggerText,
+  btnTriggerStyle,
+  showCloseBtn,
+  closeBtnText,
+  closeBtnStyle,
+  otherBtn,
+  children,
+}: Props) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className={btnTriggerStyle}>{btnTiggerText}</button>
+      </DialogTrigger>
+      <DialogContent className={`sm:max-w-${maxWidth} max-h-[90svh] overflow-auto`}>
+        {showXIcon && (
+          <DialogClose className="w-0 h-0">
+            <span className="rounded-[10px] p-1 border absolute top-3 right-3 border-gray-400 hover:border-primary transition-all duration-200 ease-in-out hover:text-primary scale-75">
+              <AiOutlineClose />
+            </span>
+          </DialogClose>
+        )}
+        {children}
+        {showCloseBtn && (
+          <div className="flex items-center justify-between gap-3">
+            <DialogClose asChild>
+              <Button variant="secondary" className={closeBtnStyle}>
+                {closeBtnText}
+              </Button>
+            </DialogClose>
+            {otherBtn}
+          </div>
+        )}
+      </DialogContent>
+    </Dialog>
+  );
+}
