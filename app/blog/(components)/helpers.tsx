53f4b88ad26be665223b6a889912e27671d8a3a5
Prosper Agwegiokhe <54757652+Phingaz@users.noreply.github.com>
2023-11-17 22:10:30 -0300

    Started blog

---
+++ app/blog/(components)/helpers.tsx
@@ -0,0 +1,41 @@
+import card from '../(assets)/card.svg';
+export const links = [
+  {
+    id: 1,
+    href: 'blog',
+    text: 'All Categories',
+  },
+  {
+    id: 2,
+    href: 'diy',
+    text: 'DIY Ideas',
+  },
+  {
+    id: 3,
+    href: 'honeymoon-and-travel',
+    text: 'Honemoon & Travel',
+  },
+  {
+    id: 4,
+    href: 'client-stores',
+    text: 'Client Stories',
+  },
+  {
+    id: 5,
+    href: 'design-and-decor',
+    text: 'Design & Decor',
+  },
+];
+
+export const data = [
+  {
+    id: 1,
+    card: card,
+    avatar: card,
+    title: 'The Ultimate Guide to Stress-Free Wedding Planning',
+    description:
+      'Dive into The Ultimate Guide to Stress-Free Wedding Planning; where we unravel the secrets to crafting',
+    name: 'John Chuka K.',
+    date: '23 Jan 2023',
+  },
+];
