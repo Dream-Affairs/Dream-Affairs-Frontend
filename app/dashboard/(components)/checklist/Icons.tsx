1ca3d316a3d4f8110823097bb5f42d11ac24a0d6
VictorIdowu <idsonvictor@gmail.com>
2023-11-20 11:03:54 +0100

    Added the submit icon for mobile form

--- app/dashboard/(components)/checklist/Icons.tsx
+++ app/dashboard/(components)/checklist/Icons.tsx
@@ -151,3 +151,26 @@ export const SearchIcon = () => {
     </svg>
   );
 };
+
+export const ArrowIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path
+        d="M14.4297 5.93L20.4997 12L14.4297 18.07"
+        stroke="#AB72C2"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.5 12H20.33"
+        stroke="#AB72C2"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
