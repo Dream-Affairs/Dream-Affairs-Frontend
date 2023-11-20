653a584d20b72c24e0269bfd63723b5bbab9465a
Khemmie-Ray <atokemmy@gmail.com>
2023-11-20 14:55:12 +0100

    adding contact page

---
+++ app/contact-us/(components)/hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import bgLogo from '../(assets)/img.svg';
+
+const Hero = () => {
+  return (
+    <section>
+      <div
+        className="bg-cover bg-center lg:h-[434px] h-[70vh] flex items-center justify-center lg:mb-[100px] mb-[40px] p-4"
+        style={{ backgroundImage: `url(${bgLogo.src})` }}
+      >
+        <div className="text-center text-white">
+          <h1 className="lg:text-[64px] text-[28px] font-[600]">Get in touch with us</h1>
+          <p className="lg:text-[24px] text-[18px] font-[500] lg:w-[791px]">We're here to assist you! Whether you have questions, feedback, or just want to say hello, we'd love to hear from you. </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
