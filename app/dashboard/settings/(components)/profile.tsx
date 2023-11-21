ecad1e727f5a0c0e69462110615bf2dcaa8c5e46
Godwin <salamigodwin01@gmail.com>
2023-11-21 20:22:09 +0100

    crediential page created

--- app/dashboard/settings/(components)/profile.tsx
+++ app/dashboard/settings/(components)/profile.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import CoverPhoto from './profile-images';
 import ProfileDetail from './profile-form';
+import Subtitle from './subtitle';
 
 const ProfileManagement = () => {
   return (
     <div>
       <CoverPhoto />
-      <div className="mt-20 sm:mt-28 md:mt-40 px-5 pe-7 md:pe-20 md:px-20">
+      <div className="mt-20 sm:mt-28 md:mt-40 px-5 pe-7 md:pe-20 md:px-16">
         <ProfileDetail />
       </div>
     </div>
