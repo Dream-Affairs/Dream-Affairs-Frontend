38eda9c631f26d6781e3cb1601fa2529a2af7dba
Prosper Agwegiokhe <54757652+Phingaz@users.noreply.github.com>
2023-12-13 21:14:45 -0300

    Fixed register api interation errors

--- app/auth/register/(forms)/two.tsx
+++ app/auth/register/(forms)/two.tsx
@@ -5,7 +5,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { DatePicker } from '../../(components)/DatePicker';
 import { Checkbox } from '@/components/ui/checkbox';
-import { isEmpty, userInfo_Token } from '../../(helpers)/helpers';
+import { isEmpty } from '../../(helpers)/helpers';
 import { toast } from '@/components/ui/use-toast';
 import axios from 'axios';
 import { FormTwoProps } from '../../(helpers)/types';
